0b7876806d873fde2f872873acf649044119ed61
Osoviet <111003277+Osoviet@users.noreply.github.com>
2022-11-07 11:49:15 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5658493659:AAGJyqCromQJZ6kqi5a4oimfWcwnTsko60k')
-API_ID =  os.environ.get('api_id','18641760')
-API_HASH = os.environ.get('api_hash','b7b026ce9d1d36400c02dc21d8df53a3')
+BOT_TOKEN =  os.environ.get('bot_token','5694666378:AAHxhtZ92GvZ199OdICwoAOdDQHhbjnK5yM')
+API_ID =  os.environ.get('api_id','17345593')
+API_HASH = os.environ.get('api_hash','80ae053901c049b519ea5bf0f50e1675')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','Yama_Tsukami').split(';')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KADKFLCAJDJJKJJGDYFASDDJHNJGDADKDGLGDELI'))
+ACCES_USERS = os.environ.get('tl_admin_user','STANISLASKI').split(';')
+PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KIDDKDYJJKJJGDYDJKGJGJYGIHIERKDGLGDELI'))
 
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
